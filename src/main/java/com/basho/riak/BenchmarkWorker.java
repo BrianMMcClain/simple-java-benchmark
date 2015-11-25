dcaab27965c7657b1dc6237935018e3fa2dde90b
Brian McClain <brianmmcclain@gmail.com>
2015-11-24 23:12:40 -0500

    Move client logic to a Runnable, basic benchmark logic prototype complete

---
+++ src/main/java/com/basho/riak/BenchmarkWorker.java
@@ -0,0 +1,80 @@
+package com.basho.riak;
+
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import com.basho.riak.client.api.RiakClient;
+import com.basho.riak.client.api.commands.timeseries.Store;
+import com.basho.riak.client.core.RiakCluster;
+import com.basho.riak.client.core.RiakNode;
+import com.basho.riak.client.core.query.timeseries.Cell;
+import com.basho.riak.client.core.query.timeseries.Row;
+
+public class BenchmarkWorker implements Runnable {
+
+	private int id;
+	private String[] hosts;
+	private int recordCount;
+	
+	private RiakClient client;
+	
+	public BenchmarkWorker(int id, String[] hosts, int recordCount) {
+		this.id = id;
+		this.hosts = hosts;
+		this.recordCount = recordCount;
+	}
+	
+	public void run() {
+		System.out.println("Started worker" + this.id + ", writing " + this.recordCount + " records");
+		
+		RiakNode.Builder builder = new RiakNode.Builder();
+    	List<RiakNode> nodes;
+    	try {
+    		nodes = RiakNode.Builder.buildNodes(builder, Arrays.asList(hosts));
+    		RiakCluster riakCluster = new RiakCluster.Builder(nodes).build();
+    		riakCluster.start();
+    		this.client = new RiakClient(riakCluster);
+    	} catch (UnknownHostException e) {
+    		e.printStackTrace();
+    	}
+    	
+    	runBenchmarkLoop();		
+    	
+    	System.out.println("worker" + this.id + " completed without error");
+	}
+	
+	private void runBenchmarkLoop() {
+		long timestamp = System.currentTimeMillis();
+		int recordsWritten = 0;
+		while (recordsWritten < this.recordCount) {
+	    	List<Row> rows = Arrays.asList(
+	    		    new Row(new Cell("family1"), 
+	    		    		new Cell("worker" + this.id), 
+	    		            Cell.newTimestamp(timestamp), 
+	    		            new Cell(1), 
+	    		            new Cell("test"),
+	    		            new Cell(1.5),
+	    		            new Cell(true)
+	    		    ));
+	
+	
+	    	Store storeCmd = new Store.Builder("GeoCheckin").withRows(rows).build();
+	    	
+			try {
+				this.client.execute(storeCmd);
+			} catch (ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			timestamp += 1;
+			recordsWritten += 1;
+    	}
+	}
+
+}
--- src/main/java/com/basho/riak/RiakJavaBenchmark.java
+++ src/main/java/com/basho/riak/RiakJavaBenchmark.java
@@ -1,35 +1,16 @@
 package com.basho.riak;
 
-import java.net.UnknownHostException;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-
-import com.basho.riak.client.api.RiakClient;
-import com.basho.riak.client.api.commands.timeseries.*;
-import com.basho.riak.client.core.RiakCluster;
-import com.basho.riak.client.core.RiakNode;
-import com.basho.riak.client.core.query.timeseries.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class RiakJavaBenchmark 
 {
+	private static String[] hosts = {"127.0.0.1"};
+	private static int recordCount = 100000;
+	private static int workerPoolSize = 32;
 	
     public static void main( String[] args )
-    {
-    	String[] hosts = {"127.0.0.1"};
-    	RiakNode.Builder builder = new RiakNode.Builder();
-    	List<RiakNode> nodes;
-    	RiakClient client = null;
-    	try {
-    		nodes = RiakNode.Builder.buildNodes(builder, Arrays.asList(hosts));
-    		RiakCluster riakCluster = new RiakCluster.Builder(nodes).build();
-    		riakCluster.start();
-    		client = new RiakClient(riakCluster);
-    	} catch (UnknownHostException e) {
-    		e.printStackTrace();
-    	}
-    	
+    {    	
 //    	myfamily varchar not null, 
 //    	myseries varchar not null, 
 //    	time timestamp not null, 
@@ -38,45 +19,24 @@ public class RiakJavaBenchmark
 //    	myfloat double not null, 
 //    	mybool boolean not null,
     	
-    	Integer totalCount = 10000;
-    	int recordsWritten = 0;
-    	long timestamp = System.currentTimeMillis();
-    	
     	long startTime = System.currentTimeMillis();
-    	while (recordsWritten < totalCount) {
-	    	List<Row> rows = Arrays.asList(
-	    		    new Row(new Cell("family1"), 
-	    		    		new Cell("series1"), 
-	    		            Cell.newTimestamp(timestamp), 
-	    		            new Cell(1), 
-	    		            new Cell("test"),
-	    		            new Cell(1.5),
-	    		            new Cell(true)
-	    		    ));
-	
-	
-	    	Store storeCmd = new Store.Builder("GeoCheckin").withRows(rows).build();
-	    	
-			try {
-				client.execute(storeCmd);
-				//System.out.println(timestamp);
-			} catch (ExecutionException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			timestamp += 1;
-			recordsWritten += 1;
+    	
+    	ExecutorService executor = Executors.newFixedThreadPool(workerPoolSize);
+    	for (int i = 0; i < workerPoolSize; i++) {
+    		Runnable worker = new BenchmarkWorker(i, hosts, recordCount / workerPoolSize);
+    		executor.execute(worker);
     	}
-    	Long endTime = System.currentTimeMillis();
+    	executor.shutdown();
+    	while(!executor.isTerminated()) {
+    		
+    	}
+    	
+    	long endTime = System.currentTimeMillis();
     	
     	Long totalTime = endTime - startTime;
-    	float recordsPerSecond = (float) (totalCount / (totalTime / 1000.0));
+    	float recordsPerSecond = (float) (recordCount / (totalTime / 1000.0));
     	
-    	System.out.println("Records Written: " + totalCount);
+    	System.out.println("Records Written: " + recordCount);
     	System.out.println("Total Run Time: " + totalTime);
     	System.out.println("Throughput: " + recordsPerSecond);
     }
