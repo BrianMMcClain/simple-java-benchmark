04e5a2439dc43d0b0c5ea94c9830b50359107cea
Brian McClain <brianmmcclain@gmail.com>
2016-06-06 11:51:05 -0400

    Script to generate riak-admin create and activate bucket commands

---
+++ scripts/generate_riak_bucket.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SIZE="${1:-10}"
+COUNT=0
+CMD="./riak-admin bucket-type create tsycsb '{\"props\": {\"n_val\": 1, \"table_def\": \"CREATE TABLE tsycsb (
+		host VARCHAR NOT NULL, 
+		worker VARCHAR NOT NULL, 
+		time TIMESTAMP NOT NULL"
+
+while [ $COUNT -lt $SIZE ]; do
+	CMD="$CMD, field$COUNT VARCHAR"
+	let COUNT=COUNT+1
+done
+
+CMD="$CMD 
+	, primary key ((host, worker, quantum(time, 10, s)), host, worker, time))\"}}';"
+
+echo $CMD
+
+echo "./riak-admin bucket-type activate tsycsb"
